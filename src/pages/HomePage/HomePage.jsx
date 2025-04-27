ebbd6e74ef16ec54df0add5bb891a0aba5d6285c
Tetiana_Zaichenko <tanja.zaychenko@gmail.com>
2025-04-27 09:52:36 +0300

    styles for MovieDetailsPage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,6 +4,7 @@ import MovieList from "../../components/MovieList/MovieList";
 const HomePage = ({ movies }) => {
   return (
     <div>
+      Trending today
       <MovieList data={movies} />
     </div>
   );
--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { fetchMovieById } from "../../services/api";
+import s from "./MovieDetailsPage.module.css";
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
@@ -28,21 +29,26 @@ const MovieDetailsPage = () => {
   }, [movieId]);
 
   return (
-    <div>
+    <div className={s.movieDetails}>
       <Link to={goBackRef.current}>Go back</Link>
       {/* <button onClick={() => navigate("/")}>Go back</button> */}
-      <h2>{movie.original_title}</h2>
-      <img
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-        alt={movie.original_title}
-      />
-      <p>Genres</p>
-      {movie.genres?.map((genre) => (
-        <span key={genre.id}>{genre.name} </span>
-      ))}
-      <p>Overview: {movie.overview}</p>
-      <p>Popularity: {movie.popularity}</p>
-      <p>Additional information</p>
+      <div className={s.movieConteiner}>
+        <img
+          className={s.img}
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.original_title}
+        />
+        <div className={s.movieInfo}>
+          <h2>{movie.original_title}</h2>
+          <h3>Genres</h3>
+          {movie.genres?.map((genre) => (
+            <span key={genre.id}>{genre.name} </span>
+          ))}
+          <h3>Overview:</h3> {movie.overview}
+          <h3>Popularity:</h3> {movie.popularity}
+        </div>
+      </div>
+      <h3>Additional information</h3>
       <nav>
         <NavLink to="cast">Cast</NavLink>
         <NavLink to="reviews">Reviews</NavLink>
