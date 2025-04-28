a2943c2234bf5f8fdfe7a5342531ee323fb519ee
Tetiana_Zaichenko <tanja.zaychenko@gmail.com>
2025-04-28 20:39:30 +0300

    add React.lazy і Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
-import HomePage from "./pages/HomePage/HomePage";
-import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import MoviesPage from "./pages/MoviesPage/MoviesPage";
-import MovieCast from "./components/MovieCast/MovieCast";
-import MovieReviews from "./components/MovieReviews/MovieReviews";
-import { useEffect, useState } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 import { fetchMovies } from "./services/api";
+import Loader from "./components/Loader/Loader";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
+const MovieDetailsPage = lazy(() =>
+  import("./pages/MovieDetailsPage/MovieDetailsPage")
+);
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
+const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
+const MovieReviews = lazy(() =>
+  import("./components/MovieReviews/MovieReviews")
+);
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -28,15 +34,20 @@ function App() {
   return (
     <div>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage movies={movies} />}></Route>
-        <Route path="/movies" element={<MoviesPage movies={movies} />}></Route>
-        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-          <Route path="cast" element={<MovieCast />}></Route>
-          <Route path="reviews" element={<MovieReviews />}></Route>
-        </Route>
-        <Route path="*" element={<NotFoundPage />}></Route>
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage movies={movies} />}></Route>
+          <Route
+            path="/movies"
+            element={<MoviesPage movies={movies} />}
+          ></Route>
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />}></Route>
+            <Route path="reviews" element={<MovieReviews />}></Route>
+          </Route>
+          <Route path="*" element={<NotFoundPage />}></Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>Loading...</div>
+  );
+};
+
+export default Loader;
